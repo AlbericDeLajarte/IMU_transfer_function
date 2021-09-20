d411e68fa237e003049c5ab14cd09fa8eac382c7
Alberic <albericlajarte@gmail.com>
2021-09-20 10:32:08 +0200

    first commit

---
+++ scripts/reader.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+import numpy as np
+from scipy.fft import fft, fftfreq
+
+import rospy
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+
+from geometry_msgs.msg import Accel
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Point
+from std_msgs.msg import Float32
+
+from mpu_6050_reader.msg import TwoDimensionalPlot
+from mpu_6050_reader.msg import TwoDimensionalPlotDatapoint
+from mpu_6050_reader.msg import Array_2D
+
+
+import time
+import board
+import adafruit_mpu6050
+
+N_SAMPLE = 40
+time_period = 25e-3
+
+#frequ = np.linspace(0.0, FREQU_LENGTH, FREQU_LENGTH*FREQU_SPACING, dtype=np.float32)
+frequ = fftfreq(N_SAMPLE, time_period)
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+
+class mpu_reader:
+
+    def __init__(self, i2c_address, imu_number):
+
+        # Class to access IMU data
+        if i2c_address == 0x68 or i2c_address == 0x69:
+            self.mpu = adafruit_mpu6050.MPU6050(i2c, i2c_address)
+
+        else:
+            self.mpu = None
+
+        # ------------- Data -------------
+
+        # Raw acceleration and gyroscope data to be published directly
+        self.accel_data = Vector3()
+        self.gyro_data = Vector3()
+
+        # Buffer of raw data
+        self.accel_data_fft_buffer = Array_2D()
+        self.gyro_data_fft_buffer = Array_2D()
+
+        # Computed FFT from buffer data
+        self.accel_data_fft = Array_2D()
+        self.gyro_data_fft = Array_2D()
+
+        # ------------- Publishers -------------
+
+        # Raw data topics
+        self.acceleration_pub = rospy.Publisher('acceleration_'+str(imu_number), Vector3, queue_size=10)
+        self.gyro_pub = rospy.Publisher('gyro_'+str(imu_number), Vector3, queue_size=10)
+
+        # FFT topics
+        self.frequency_fft_pub = rospy.Publisher('freq_fft_'+str(imu_number), numpy_msg(Floats), queue_size=10)
+        self.acceleration_fft_pub = rospy.Publisher('accel_fft_'+str(imu_number), numpy_msg(Array_2D), queue_size=10)
+        self.gyroscope_fft_pub = rospy.Publisher('gyro_fft_'+str(imu_number), numpy_msg(Array_2D), queue_size=10)
+
+
+    def handle_raw_data(self, i):
+
+        # Get raw data
+        if self.mpu != None:
+            self.accel_data.x, self.accel_data.y, self.accel_data.z = self.mpu.acceleration
+            self.gyro_data.x, self.gyro_data.y, self.gyro_data.z = self.mpu.gyro
+
+        # Publish raw data
+        self.acceleration_pub.publish(self.accel_data)
+        self.gyro_pub.publish(self.gyro_data)
+
+        # Fill buffer
+        self.accel_data_fft_buffer.x[i], self.accel_data_fft_buffer.y[i], self.accel_data_fft_buffer.z[i] = (self.accel_data.x, self.accel_data.y, self.accel_data.z)
+        self.gyro_data_fft_buffer.x[i], self.gyro_data_fft_buffer.y[i], self.gyro_data_fft_buffer.z[i] = (self.gyro_data.x, self.gyro_data.y, self.gyro_data.z)
+
+
+    def handle_fft(self):
+
+        # Compute FFT from buffer data
+        self.accel_data_fft.x, self.accel_data_fft.y, self.accel_data_fft.z = ( np.abs(fft(self.accel_data_fft_buffer.x)) , np.abs(fft(self.accel_data_fft_buffer.y)), np.abs(fft(self.accel_data_fft_buffer.z)) )
+        self.gyro_data_fft.x, self.gyro_data_fft.y, self.gyro_data_fft.z = ( np.abs(fft(self.gyro_data_fft_buffer.x)) , np.abs(fft(self.gyro_data_fft_buffer.y)), np.abs(fft(self.gyro_data_fft_buffer.z)) )
+
+        # print(frequ)
+        # print("")
+        
+        # Publish FFT
+        self.frequency_fft_pub.publish( frequ )
+        self.acceleration_fft_pub.publish( self.accel_data_fft )
+        self.gyroscope_fft_pub.publish( self.gyro_data_fft )
+
+
+
+def reader():
+
+    IMU_IN = mpu_reader(0x69, 1)
+    IMU_OUT = mpu_reader(0x68, 2)
+    
+    rospy.init_node('reader', anonymous=True)
+    rate = rospy.Rate(1/time_period) # 100hz
+
+    accel_tf = Array_2D()
+    gyro_tf = Array_2D()
+
+    accel_transfer_function_pub = rospy.Publisher('accel_tf', numpy_msg(Array_2D), queue_size=10)
+    gyro_transfer_function_pub = rospy.Publisher('gyro_tf', numpy_msg(Array_2D), queue_size=10)
+
+
+    # Infinite loop: continously sample, compute FFT and send data
+    i = 0
+    while not rospy.is_shutdown():
+
+        IMU_IN.handle_raw_data(i)
+        IMU_OUT.handle_raw_data(i)
+
+        # Compute FFT only when buffer is full
+        if i == N_SAMPLE-1:
+            i = 0
+            IMU_IN.handle_fft()
+            IMU_OUT.handle_fft()
+
+            accel_tf.x, accel_tf.y, accel_tf.z = ( IMU_OUT.accel_data_fft.x/IMU_IN.accel_data_fft.x, 
+                                                   IMU_OUT.accel_data_fft.y/IMU_IN.accel_data_fft.y,
+                                                  IMU_OUT.accel_data_fft.z/IMU_IN.accel_data_fft.z, )
+
+            gyro_tf.x, gyro_tf.y, gyro_tf.z = ( IMU_OUT.gyro_data_fft.x/IMU_IN.gyro_data_fft.x, 
+                                                IMU_OUT.gyro_data_fft.y/IMU_IN.gyro_data_fft.y,
+                                                IMU_OUT.gyro_data_fft.z/IMU_IN.gyro_data_fft.z, )
+
+            
+            accel_transfer_function_pub.publish( accel_tf)
+            gyro_transfer_function_pub.publish(gyro_tf)
+
+
+        i += 1
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        reader()
+    except rospy.ROSInterruptException:
+        pass
