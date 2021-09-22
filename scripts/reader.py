bf19b9efee039b918df2479dd998e36025456d33
AlbericLaj <albericlajarte@gmail.com>
2021-09-22 13:23:08 +0200

    removed FFT + added config for speed improvment

--- scripts/reader.py
+++ scripts/reader.py
@@ -22,8 +22,22 @@ import time
 import board
 import adafruit_mpu6050
 
+use_acc = rospy.get_param("accelerometer")
+use_gyro = rospy.get_param("gyroscope")
+do_fft = rospy.get_param("real_time_fft")
+
+IMU_calib = [
+    {"acc_gain": np.array([0.9997, 0.9983, 1.0242]), "acc_bias": np.array([0.5608, -0.0070, -1.7242]),
+     "gyro_gain": np.array([0.9871, 0.9907, 1.0292]), "gyro_bias": np.array([-0.0073, -0.0064, -0.0315])
+    },
+
+    {"acc_gain": np.array([0.9947, 1.0001, 1.0150]), "acc_bias": np.array([0.4469, 0.0836, -0.1983]),
+     "gyro_gain": np.array([0.9666, 1.0254, 1.0084]), "gyro_bias": np.array([-0.0467, -0.0239, -0.0348])
+    }
+]
+
 N_SAMPLE = 40
-time_period = 25e-3
+time_period = 8e-3
 
 #frequ = np.linspace(0.0, FREQU_LENGTH, FREQU_LENGTH*FREQU_SPACING, dtype=np.float32)
 frequ = fftfreq(N_SAMPLE, time_period)
@@ -41,6 +55,8 @@ class mpu_reader:
 
         else:
             self.mpu = None
+
+        self.imu_number = imu_number
 
         # ------------- Data -------------
 
@@ -63,28 +79,42 @@ class mpu_reader:
         self.gyro_pub = rospy.Publisher('gyro_'+str(imu_number), Vector3, queue_size=10)
 
         # FFT topics
-        self.frequency_fft_pub = rospy.Publisher('freq_fft_'+str(imu_number), numpy_msg(Floats), queue_size=10)
-        self.acceleration_fft_pub = rospy.Publisher('accel_fft_'+str(imu_number), numpy_msg(Array_2D), queue_size=10)
-        self.gyroscope_fft_pub = rospy.Publisher('gyro_fft_'+str(imu_number), numpy_msg(Array_2D), queue_size=10)
+        if do_fft:
+            self.frequency_fft_pub = rospy.Publisher('freq_fft_'+str(imu_number), numpy_msg(Floats), queue_size=10)
+            self.acceleration_fft_pub = rospy.Publisher('accel_fft_'+str(imu_number), numpy_msg(Array_2D), queue_size=10)
+            self.gyroscope_fft_pub = rospy.Publisher('gyro_fft_'+str(imu_number), numpy_msg(Array_2D), queue_size=10)
 
 
     def handle_raw_data(self, i):
 
-        # Get raw data
+        # Get raw data and publish them
         if self.mpu != None:
-            self.accel_data.x, self.accel_data.y, self.accel_data.z = self.mpu.acceleration
-            self.gyro_data.x, self.gyro_data.y, self.gyro_data.z = self.mpu.gyro
 
-        # Publish raw data
-        self.acceleration_pub.publish(self.accel_data)
-        self.gyro_pub.publish(self.gyro_data)
+            if use_acc:
+                acceleration = np.array(self.mpu.acceleration)
+                acceleration = (acceleration - IMU_calib[self.imu_number-1]["acc_bias"])/IMU_calib[self.imu_number-1]["acc_gain"]
 
+                self.accel_data.x, self.accel_data.y, self.accel_data.z = tuple(acceleration)
+                self.acceleration_pub.publish(self.accel_data)
+
+            if use_gyro:
+                gyro = np.array(self.mpu.gyro)
+                gyro = (gyro - IMU_calib[self.imu_number-1]["gyro_bias"])/IMU_calib[self.imu_number-1]["gyro_gain"]
+
+                self.gyro_data.x, self.gyro_data.y, self.gyro_data.z = tuple(gyro)
+                self.gyro_pub.publish(self.gyro_data)
+        
+        
         # Fill buffer
-        self.accel_data_fft_buffer.x[i], self.accel_data_fft_buffer.y[i], self.accel_data_fft_buffer.z[i] = (self.accel_data.x, self.accel_data.y, self.accel_data.z)
-        self.gyro_data_fft_buffer.x[i], self.gyro_data_fft_buffer.y[i], self.gyro_data_fft_buffer.z[i] = (self.gyro_data.x, self.gyro_data.y, self.gyro_data.z)
+        if do_fft:
+            self.accel_data_fft_buffer.x[i], self.accel_data_fft_buffer.y[i], self.accel_data_fft_buffer.z[i] = (self.accel_data.x, self.accel_data.y, self.accel_data.z)
+            self.gyro_data_fft_buffer.x[i], self.gyro_data_fft_buffer.y[i], self.gyro_data_fft_buffer.z[i] = (self.gyro_data.x, self.gyro_data.y, self.gyro_data.z)
 
 
     def handle_fft(self):
+
+        if not do_fft:
+            return
 
         # Compute FFT from buffer data
         self.accel_data_fft.x, self.accel_data_fft.y, self.accel_data_fft.z = ( np.abs(fft(self.accel_data_fft_buffer.x)) , np.abs(fft(self.accel_data_fft_buffer.y)), np.abs(fft(self.accel_data_fft_buffer.z)) )
@@ -106,13 +136,14 @@ def reader():
     IMU_OUT = mpu_reader(0x68, 2)
     
     rospy.init_node('reader', anonymous=True)
-    rate = rospy.Rate(1/time_period) # 100hz
+    rate = rospy.Rate(1/time_period) # Sampling rate in Hz
 
-    accel_tf = Array_2D()
-    gyro_tf = Array_2D()
+    if do_fft:
+        accel_tf = Array_2D()
+        gyro_tf = Array_2D()
 
-    accel_transfer_function_pub = rospy.Publisher('accel_tf', numpy_msg(Array_2D), queue_size=10)
-    gyro_transfer_function_pub = rospy.Publisher('gyro_tf', numpy_msg(Array_2D), queue_size=10)
+        accel_transfer_function_pub = rospy.Publisher('accel_tf', numpy_msg(Array_2D), queue_size=10)
+        gyro_transfer_function_pub = rospy.Publisher('gyro_tf', numpy_msg(Array_2D), queue_size=10)
 
 
     # Infinite loop: continously sample, compute FFT and send data
@@ -122,23 +153,24 @@ def reader():
         IMU_IN.handle_raw_data(i)
         IMU_OUT.handle_raw_data(i)
 
-        # Compute FFT only when buffer is full
-        if i == N_SAMPLE-1:
-            i = 0
-            IMU_IN.handle_fft()
-            IMU_OUT.handle_fft()
+        if do_fft:
+            #Compute FFT only when buffer is full
+            if i == N_SAMPLE-1:
+                i = 0
+                IMU_IN.handle_fft()
+                IMU_OUT.handle_fft()
 
-            accel_tf.x, accel_tf.y, accel_tf.z = ( IMU_OUT.accel_data_fft.x/IMU_IN.accel_data_fft.x, 
-                                                   IMU_OUT.accel_data_fft.y/IMU_IN.accel_data_fft.y,
-                                                  IMU_OUT.accel_data_fft.z/IMU_IN.accel_data_fft.z, )
+                accel_tf.x, accel_tf.y, accel_tf.z = ( IMU_OUT.accel_data_fft.x/IMU_IN.accel_data_fft.x, 
+                                                    IMU_OUT.accel_data_fft.y/IMU_IN.accel_data_fft.y,
+                                                    IMU_OUT.accel_data_fft.z/IMU_IN.accel_data_fft.z, )
 
-            gyro_tf.x, gyro_tf.y, gyro_tf.z = ( IMU_OUT.gyro_data_fft.x/IMU_IN.gyro_data_fft.x, 
-                                                IMU_OUT.gyro_data_fft.y/IMU_IN.gyro_data_fft.y,
-                                                IMU_OUT.gyro_data_fft.z/IMU_IN.gyro_data_fft.z, )
+                gyro_tf.x, gyro_tf.y, gyro_tf.z = ( IMU_OUT.gyro_data_fft.x/IMU_IN.gyro_data_fft.x, 
+                                                    IMU_OUT.gyro_data_fft.y/IMU_IN.gyro_data_fft.y,
+                                                    IMU_OUT.gyro_data_fft.z/IMU_IN.gyro_data_fft.z, )
 
-            
-            accel_transfer_function_pub.publish( accel_tf)
-            gyro_transfer_function_pub.publish(gyro_tf)
+                
+                accel_transfer_function_pub.publish( accel_tf)
+                gyro_transfer_function_pub.publish(gyro_tf)
 
 
         i += 1
